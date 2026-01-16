705654735b1e4e9911544105fa7ce9f41b9562b0
Rahul TS <tsrahul1996@gmail.com>
2026-01-16 13:21:41 +0000

    initial commit

---
+++ src/main/java/com/spirngboot/spirngboot/usermanagement/SpirngbootUsermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.spirngboot.spirngboot.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootUsermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngbootUsermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spirngboot/spirngboot/usermanagement/SpirngbootUsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spirngboot.spirngboot.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngbootUsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
