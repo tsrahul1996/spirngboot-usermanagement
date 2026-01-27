87a16e6dc3ea36b022e6448a97f6cb72a70734bd
Rahul TS <tsrahul1996@gmail.com>
2026-01-27 11:53:12 +0000

    delete user by Id implemented.

--- src/main/java/com/spirngboot/spirngboot/usermanagement/service/UserService.java
+++ src/main/java/com/spirngboot/spirngboot/usermanagement/service/UserService.java
@@ -2,9 +2,19 @@ package com.spirngboot.spirngboot.usermanagement.service;
 
 import com.spirngboot.spirngboot.usermanagement.dto.UserDto;
 
+import java.util.List;
+
 public interface UserService {
 
     UserDto createUser(UserDto userDto);
 
     UserDto getUserById(Long id);
+
+    List<UserDto> getAllUsers();
+
+    UserDto updateUser(Long id, UserDto userDto);
+
+    void deleteUser(Long id);
+
+
 }
--- src/main/java/com/spirngboot/spirngboot/usermanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/spirngboot/spirngboot/usermanagement/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.spirngboot.spirngboot.usermanagement.repository.UserRepository;
 import com.spirngboot.spirngboot.usermanagement.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,5 +37,50 @@ public class UserServiceImpl implements UserService {
 
 
         return userMapper.toDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+
+        return userList.stream()
+                .map(userMapper::toDto)
+                .toList();
+
+    }
+
+    @Override
+    public UserDto updateUser(Long id, UserDto userDto) {
+//        User savedUser = null;
+//        if(userRepository.existsById(id)){
+//
+//         userRepository.deleteById(id);
+//            savedUser = userRepository.save(userMapper.toEntity(userDto));
+//
+//        }
+//
+//        return userMapper.toDto(savedUser);
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+id));
+
+        user.setFirstName(userDto.firstName());
+        user.setLastName(userDto.lastName());
+        user.setEmail(userDto.lastName());
+
+        User savedUser = userRepository.save(user);
+
+        return userMapper.toDto(savedUser);
+
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+id));
+
+        userRepository.deleteById(id);
+
     }
 }
